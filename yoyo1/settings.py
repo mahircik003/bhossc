aa7500a024a42dfc55d9ff8958bf24f21221b21a
Kamran Mekhtiyev <kamranmehdiyev02@gmail.com>
2022-06-09 15:50:45 +0400

    nc

--- yoyo1/settings.py
+++ yoyo1/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #os.environ.get('DEBUG') == 'True' # os.environ.get("DEBUG", "False") == "True" 
+DEBUG = True #os.environ.get('DEBUG') == 'True' # os.environ.get("DEBUG", "False") == "True" 
 
 ALLOWED_HOSTS = ['bhos.svdev.me', 'admin.svdev.me', '127.0.0.1', 'localhost', '.herokuapp.com', 'bhossc.herokuapp.com']
 
